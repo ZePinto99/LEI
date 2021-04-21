a2f50d7aa3bdc13dfe4aa2a0c87e8e03b864a518
EduardoCosta99 <50023394+EduardoCosta99@users.noreply.github.com>
2021-04-21 17:01:42 +0100

    bd

--- Database/test.sql
+++ Database/test.sql
@@ -1,1 +1,22 @@
-Insert into template VALUes (DEFAULT, 'O "TITULO_JOG" do "CLUBE" assinalou no ultimo jogo a sua presenca numero "NR_JOGOS_JOG". "NOME_JOG" estreou-se a "ESTREIA_JOG", numa partida onde o "CLUBE" enfrentou o "ADVERSARIO", com um resultado final de "RESULTADO_ESTREIA".', 100, 1,1);
+/************** DELETES **************/
+DELETE FROM version;
+DELETE FROM keywords;
+DELETE FROM template;
+DELETE FROM history;
+
+/**************  RESET AUTOINCREMENT IDs **************/
+ALTER TABLE version AUTO_INCREMENT = 1;
+ALTER TABLE keywords AUTO_INCREMENT = 1;
+ALTER TABLE template AUTO_INCREMENT = 1;
+ALTER TABLE history AUTO_INCREMENT = 1;
+
+/**************  POVOAMENTO **************/ 
+INSERT INTO version VALUES (DEFAULT,1);
+INSERT INTO keywords VALUES (DEFAULT,1,0,1,0,1,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0);
+INSERT INTO template VALUES (DEFAULT, 1, 1, 34, 'O "POS_JOG" do "CLUBE" assinalou no ultimo jogo a sua presenca numero "NR_JOGOS_JOG". "NOME_JOG" estreou-se a "ESTREIA_JOG", numa partida onde o "CLUBE" enfrentou o "ADVERSARIO", com um resultado final de "RESULTADO_ESTREIA".');
+
+
+/**************  QUERY TESTING **************/ 
+SELECT text, keywords, id_template, size FROM template, version WHERE template.version = id_version AND version.type = 1;
+SELECT * FROM keywords WHERE id_keywords = 1;
+INSERT INTO history VALUES (DEFAULT, NOW(), 'blablabla', 1, null, null, null, null);
