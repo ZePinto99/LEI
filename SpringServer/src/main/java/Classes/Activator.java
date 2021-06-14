6f46de9a913ac0dbe530a90ab022d9627a6bc145
José Pinto <z.miguelpinto@gmail.com>
2021-06-14 12:45:40 +0100

    nonRepeatableKeywords

--- SpringServer/src/main/java/Classes/Activator.java
+++ SpringServer/src/main/java/Classes/Activator.java
@@ -13,27 +13,40 @@ public class Activator {
     }
 
     //função que calcula o score de um template
-    public double tempScore(List<String> templateKeywords, Map<String, Integer> newsKeywords){
+    //o templates score será a junção de todos da relação co todos os templates
+    //isto vai implicar ao inserirmos cada template termos um de fazer puts às relações com templates que não existiam e atualizar os valore
+    //de registos que já existem
+    public double tempScore(List<String> templateKeywords, Map<String, Integer> newsKeywords){//List<Integer> templatesUsed, Map <Integer, Integer> templatesScores){
         int score = 0;
 
-        //set de priority keywords da notícia (todas as priority keywords diferentes de cada keyword da notícia final)
-        //Set priorityKeywordsSet =  priorityKeywordsSet(newsKeywords.keySet());
+        //Não permite fazer escolher templates com certas keywords
+        if(nonRepeatableKeywords(templateKeywords, newsKeywords))
+            return 0;
 
         //para cada keyword do template
         for(String templateKeyword :  templateKeywords){
 
             int occurrences = 0;
+
+            //keywords que não se podem repetir
+
             //Vai ver quantas vezes a a templateKeyword aparece na notícia final
             if(newsKeywords.containsKey(templateKeyword))
                 occurrences = newsKeywords.get(templateKeyword);
             //penaliza a keyword por cada repetição
             score -= constant*occurrences;
+        }
 
-            //Vai ver se a templateKeyword é uma priority keyword
-            //nota: cada priority keyword só é contabilizada 1 vez
-            //aumenta o score caso se tarte de uma priority keyword
-            //score += priorityScore(templateKeyword, priorityKeywordsSet);
+        //percorre o mapa de templateScores e vê a relação do template com os templates que já pertencem à notícia
+        /*
+        for (int usedTemplate : templatesUsed) {
+            try{
+                score += priorityScore(templateId, templatesScores);
+            }catch (Exception e){
+                score += 0;
             }
+        }
+        */
 
         //devolve um score/probabilidade entre 0 e 1
         double activationValue = sigmoid(score);
@@ -41,26 +54,14 @@ public class Activator {
         return activationValue;
     }
 
-    //Função que cria um Set com todas as as priority keywords da notícia
-    private Set<String> priorityKeywordsSet(Set<String> newsKeywords){
-        //Escolhi um set para só contabilizarmos uma vez cada priority key
-        Set<String> prioritykeys = new HashSet<>();
-
-        for (String newsKeyword : newsKeywords){
-            /////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ////////////////////////
-            //Temos de ir buscar as priority keywords de cada keyword do template
-            List<String> prioritykey = new ArrayList<>();
-            /////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ////////////////////////
-            prioritykeys.addAll(prioritykey);
+    //Dá zero se templateId não tiver no mapa de classificações
+    public int priorityScore(String templateId, Map <Integer, Integer> templatesScores){
+        int occures;
+        try {
+            occures = templatesScores.get(templateId);
+        }catch (Exception e){
+            occures = 0;
         }
-        return prioritykeys;
-    }
-
-    //Dá zero se templateKeyword não for priority e constant se for
-    public int priorityScore(String templateKeyword, Set<String> priorykeys){
-        int occures = 0;
-        if(priorykeys.contains(templateKeyword))
-            occures = 1;
         return constant*occures;
     }
 
@@ -68,6 +69,21 @@ public class Activator {
     public double sigmoid(int score){
         double x = (score)*0.1; // +feedBack -> + prob   +nrUses -> - prob
         return 1/(1 + Math.pow(this.e, -x));
+    }
+
+    //Check for non Repeatable Keywords
+    public boolean nonRepeatableKeywords(List<String> templateKeywords, Map<String, Integer> newsKeywords){
+
+        Set<String> nonRepeatableKeywords = new HashSet();
+        List<String> objs = Arrays.asList("IDADE_JOG", "NAC_JOG", "CLUBE", "TREINADOR", "ARBITRO", "JORNADA", "RESULTADO_JOGO", "CASA/FORA", "ADVERSARIO", "ESTADIO", "LOCALIZACAO", "MARCADOR_JORNADA", "ESTREIA_JOG", "RESULTADO_ESTREIA");
+        nonRepeatableKeywords.addAll(objs);
+        Set<String> templateNonRepeatableKeywords = new HashSet<>();
+        for (String templateKeyword : templateKeywords){
+            if (nonRepeatableKeywords.contains(templateKeyword))
+                templateNonRepeatableKeywords.add(templateKeyword);
+        }
+
+        return  newsKeywords.keySet().stream().anyMatch(templateNonRepeatableKeywords::contains);
     }
 }
 
